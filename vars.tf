ad7205eee85756c20c942496abef3e80d48132b8
saidevops7722 <saiaws7722@gmail.com>
2024-03-13 21:45:50 +0530

    jenkins infra

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "name" {}
+variable "tags" {}
+variable "env" {}
+variable "internal" {}
+variable "load_balancer_type" {}
+variable "enable_deletion_protection" {
+  default = false
+}
+variable "subnets" {}
